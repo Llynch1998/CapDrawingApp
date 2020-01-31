dc09de4f1c3b7509a3746cc1a16f71513b1c1d6b
JordynA97 <jla5483@rit.edu>
2020-01-31 13:13:46 -0500

    what is happening

--- src/server.js
+++ src/server.js
@@ -1,1 +1,20 @@
 console.log("hi");
+
+var http = require('http');
+var fs = require('fs');
+
+const port = process.env.PORT || process.end.NODE_PORT || 3000;
+
+
+fs.readFile('./draw-and-share-start.html', function (err, html){
+    if (err){
+        throw err;
+    }
+
+    http.createServer(function (err, html){
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+
+    }).listen(port);
+});
